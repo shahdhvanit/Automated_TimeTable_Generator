3b9950a6492741a85e689a2b65510e985bc2109e
Pranshu Patel <pranshu05patel@gmail.com>
2024-03-05 21:48:17 +0530

    Add rooms class

---
+++ rooms.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Room {
+    public:
+        int roomNumber;
+        int seating;
+        string roomType;
+        string remarks;
+};
